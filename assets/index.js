989b9a13167dad5790ad1425063fba193c2e0ced
Tonoobvill <86608694+Tonoobvill@users.noreply.github.com>
2024-12-13 10:32:33 -0500

    First upload

---
+++ assets/index.js
@@ -0,0 +1,12 @@
+// Wrap every letter in a span
+var textWrapper = document.querySelector('#logo .letters');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: false})
+  .add({
+    targets: '#logo .letter',
+    scale: [0, 1],
+    duration: 1500,
+    elasticity: 600,
+    delay: (el, i) => 45 * (i+1)
+  })
